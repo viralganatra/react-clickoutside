c324163fa89a54c8597330535764a73776760467
Viral Ganatra <viral@viralganatra.com>
2016-11-25 13:57:42 +0000

    Implement functionality to register if a click occured outside of the decorated component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default function ClickOutsideHOC(WrappedComponent) {
+    const EVENT = 'mousedown';
+
     function getDisplayName() {
         return WrappedComponent.displayName || WrappedComponent.name || 'Component';
     }
@@ -8,9 +10,41 @@ export default function ClickOutsideHOC(WrappedComponent) {
     return class ClickOutside extends Component {
         static displayName = `withClickOutsideHOC(${getDisplayName()})`;
 
+        static propTypes = {
+            onClickOutside: PropTypes.func.isRequired,
+        }
+
+        componentDidMount() {
+            global.addEventListener(EVENT, this.onClickOutside);
+        }
+
+        componentWillUnmount() {
+            global.removeEventListener(EVENT, this.onClickOutside);
+        }
+
+        hasClickedOutside(containerNode, targetNode) {
+            return !containerNode.contains(targetNode);
+        }
+
+        onClickOutside = ({ target }) => {
+            const { containerNode, hasClickedOutside } = this;
+
+            if (containerNode && hasClickedOutside(containerNode, target)) {
+                this.props.onClickOutside();
+            }
+        }
+
+        containerRef = (node) => {
+            this.containerNode = node;
+        }
+
         render() {
+            const { onClickOutside, ...rest } = this.props;
+
             return (
-                <WrappedComponent {...this.props} />
+                <div ref={this.containerRef}>
+                    <WrappedComponent {...rest} />
+                </div>
             );
         }
     };
